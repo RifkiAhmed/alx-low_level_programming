3122ae329da2e79da32f5c08aad65bfc234cc554
Seeker <rifkiahmed.91@gmail.com>
2023-03-16 19:35:55 +0000

    Write a C program that use printf function to prints a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - "prints with proper grammar, but the outcome is a piece of art,"
+ * printf function used
+ * Return: vlue 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
