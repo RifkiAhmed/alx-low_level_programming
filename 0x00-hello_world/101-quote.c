7df33aa619b3db22a16e8d65f667d3f522a13d51
Seeker <rifkiahmed.91@gmail.com>
2023-03-16 21:16:09 +0000

    Write a C program that prints a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/**
+ * main - print "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * Return: value 1
+ */
+
+int main(void)
+{
+	write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n", 59);
+	return (1);
+}
