a0ed11db6281ec303f05e88233e12e3889fbfa84
Seeker <rifkiahmed.91@gmail.com>
2023-03-17 19:34:49 +0000

    Print alphabet in lowercase with putchar function

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,14 @@
 
 /**
  * main - print alphabet in lower case with putchar
- */
+ * Return: 0 end of program
+ * /
 
 int main(void)
 {
 	int i;
 
-	for (i=97; i<=122; i++)
+	for (i = 97; i <= 122; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
