0351dd6ecf4d4212c82e551b464bb9258a3f34b1
Seeker <rifkiahmed.91@gmail.com>
2023-03-17 20:12:44 +0000

    Prints all single digit numbers of base 10 starting from 0 with putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0 with putchar
+ * Return: 0 end of program
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
