03e30126dd8995550fd227da99e0c36c1b632044
Seeker <rifkiahmed.91@gmail.com>
2023-03-17 17:12:46 +0000

    Create program that check if a number is positive, negative ornull

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,21 +5,23 @@
 /**
  * main - check if the number n is positive, negative or null
  * @n: number to be checked
- *
+ * 
  * Return: 0
  */
 
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
+
 	return (0);
 }
