92bc38cec5291b08e14b2dfdaae1f6462d6b50bd
AhmedSeeker <rifkiahmed.91@gmail.com>
2023-03-17 04:32:43 -0700

    Check out if the value of variable n is positive,  negative or null

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,12 +7,11 @@
  * main  - print whether the value of  n is positive, negative or null
  * @n: the number to be checked
  *
- * Return: 0, end of programm
+ * Return: end of program return zero
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
