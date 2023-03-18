8ba7371f497b30745d302e7578aaac79e1821c6a
Seeker <rifkiahmed.91@gmail.com>
2023-03-18 02:12:29 +0000

    Prints all possible different combinations of two two_digits numbers separated by ','

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,15 +12,11 @@ int main(void)
 	int i, j, k, l;
 
 	for (i = 48 ; i <= 57 ; i++)
-		for (j = 48 ; j <= 57 ; j++)
+		for (j = 48 ; j <= 56 ; j++)
 			for (k = 48 ; k <= 57 ; k++)
 				for (l = 48 ; l <= 57 ; l++)
 				{
-					if (i >= k && j >= l)
-					{
-						continue;
-					}
-					else
+					if (k > i || (k == i && l > j))
 					{
 						putchar(i);
 						putchar(j);
@@ -28,7 +24,12 @@ int main(void)
 						putchar(k);
 						putchar(l);
 					}
-					if (i < 57 && j < 56)
+					else
+					{
+						continue;
+					}
+
+					if (!(i == 57 && j == 56))
 					{
 						putchar(',');
 						putchar(' ');
