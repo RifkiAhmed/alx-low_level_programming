ef61de239635231b902079cc5f9e7a6892362167
Seeker <rifkiahmed.91@gmail.com>
2023-03-22 15:06:05 +0000

    Fix the code to out the largest of three numbers, no matter the case

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,16 +12,16 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a >= b)
 	{
-		if (a > c)
+		if (a >= c)
 			largest = a;
 		else
 			largest = c;
 	}
 	else if (b > a)
 	{
-		if (b > c)
+		if (b >= c)
 			largest = b;
 		else
 			largest = c;
