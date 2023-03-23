1b0d9de0c47950e84047f37cadea45007b7fe22a
Seeker <rifkiahmed.91@gmail.com>
2023-03-23 23:57:58 +0000

    Prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 1 ; i <= size ; i++)
+	{
+		for (j = i ; j < size ; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0 ; k < i ; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
