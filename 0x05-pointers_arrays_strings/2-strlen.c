0c3915f48aa1094af5299cdf87f7e51496a96d48
Seeker <rifkiahmed.91@gmail.com>
2023-03-27 22:26:35 +0000

    Return length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * _strlen - return the lenght of a string
+ *@s: value of the pointer to 's'
+ *
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
