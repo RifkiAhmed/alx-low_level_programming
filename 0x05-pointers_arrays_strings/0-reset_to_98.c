6480f6b8deb3971950d0d299cc82bffb6ec8164b
Seeker <rifkiahmed.91@gmail.com>
2023-03-27 22:05:15 +0000

    Update the value of a pointer that it point to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+#include <unistd.h>
+
+/**
+ * reset_to_98 - updates the value of a pointer that it points to to 98
+ *@n: value of the pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
