60d6fe7dab2db1ddf5668d63f6f13bb28d050a8d
Seeker <rifkiahmed.91@gmail.com>
2023-03-27 22:59:37 +0000

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,10 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = strlen(s) ; i >= 0 ; i--)
-		_putchar(s[i]);
-	_putchar('\n');
+	if (strlen != 0)
+	{
+		for (i = strlen(s) ; i >= 0 ; i--)
+			_putchar(s[i]);
+		_putchar('\n');
+	}
 }
