1c2e323e87c23277d1521584d988290f7738f607
Seeker <rifkiahmed.91@gmail.com>
2023-03-28 01:57:58 +0000

    Prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	len = strlen(str);
 	if (len % 2 == 0)
 	{
-		for (i = len / 2 + 1; i < (int)len ; i++)
+		for (i = len / 2 ; i < (int)len ; i++)
 			_putchar(*(str + i));
 	}
 	else
 	{
-		for (i = (len - 1)  / 2 ; i < (int)len ; i++)
+		for (i = (len - 1)  / 2 ; i < (int)len - 1 ; i++)
 			_putchar(*(str + i));
 
 	}
