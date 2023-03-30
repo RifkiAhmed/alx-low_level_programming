1df026f3d0f021f5de77a70fb3201a8015cfb3e5
Seeker <rifkiahmed.91@gmail.com>
2023-03-30 22:35:25 +0000

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,10 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-				;
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
 	return (dest);
 }
