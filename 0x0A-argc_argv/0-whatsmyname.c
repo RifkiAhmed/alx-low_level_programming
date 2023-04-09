81b7e90af97bc7d1346d465859d2b34f2e36d9e4
Seeker <rifkiahmed.91@gmail.com>
2023-04-09 03:41:46 +0000

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
- 
+
 /**
   * main - prints the name of program
+  * @argc: number of arguements passed to the program
+  * @argv: array of strings of each arguments passed to the program
   *
   * Return: always 0
   */
