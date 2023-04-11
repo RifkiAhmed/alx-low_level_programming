8587ed846a87f84f906a3bff1c2cb5a2bdfe6846
Seeker <rifkiahmed.91@gmail.com>
2023-04-11 22:09:08 +0000

    creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,16 +19,21 @@ char *create_array(unsigned int size, char c)
 	char *array;
 
 	if (size == 0)
-		return ("NULL");
-	array = malloc(size);
-	if (array == NULL)
 	{
-		return ("NULL\n");
+		return ("NULL");
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
-			array[i] = c;
-		return (array);
+		array = malloc(size);
+		if (array == NULL)
+		{
+			return ("NULL\n");
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+			return (array);
+		}
 	}
 }
