7107970e1f7ed5bf2fa21d9e8b015904222415fb
Seeker <rifkiahmed.91@gmail.com>
2023-04-27 21:03:01 +0000

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to list_t
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,17 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+
 #include <stddef.h>
+
 /**
- * struct list - singly linked list
+ * struct list_s - singly linked list
  * @str: string
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list struct
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -15,7 +19,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -23,4 +26,3 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
-
