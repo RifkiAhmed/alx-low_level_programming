a73090a7d7b1bd8815ffa6f0be835c9752032700
ahmed <rifkiahmed.91@gmail.com>
2023-05-02 18:25:21 +0000

    Function that add an element at the beginning pf a linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,6 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *ptr;
 
 	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
 	ptr->n = n;
 	ptr->next = *head;
 	*head = ptr;
