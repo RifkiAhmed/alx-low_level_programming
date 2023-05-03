51554d36064c4452b13405421491cb40971d7e31
ahmed <rifkiahmed.91@gmail.com>
2023-05-03 00:12:39 +0000

    Function that inserts a node in linked list at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr, *tmp;
 
 	ptr = malloc(sizeof(listint_t));
-	if (!ptr || !(*head))
+	if (ptr == NULL)
+		return (NULL);
+
+	if (*head == NULL)
 		return (NULL);
 
 	ptr->n = n;
