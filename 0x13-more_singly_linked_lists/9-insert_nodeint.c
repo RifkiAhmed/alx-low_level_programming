cd6cdbac9efeed3d60e6becf2a503d673b605f8c
ahmed <rifkiahmed.91@gmail.com>
2023-05-03 00:27:57 +0000

    Function that inserts a node in linked list at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	count = 0;
 
-	while (tmp->next != NULL)
+	while (tmp->next != NULL || count < (idx -1))
 	{
 		tmp = tmp->next;
 		count++;
