28bc8712a774d3e657f9427c7684787defd5e178
ahmed <rifkiahmed.91@gmail.com>
2023-05-03 01:59:12 +0000

    fix check errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	ptr->n = n;
