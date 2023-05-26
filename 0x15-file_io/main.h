66baf459ca9ff5fe7a1908d3d96a1b86cd8c8981
ahmed <rifkiahmed.91@gmail.com>
2023-05-26 19:33:48 +0000

    Add task 2 function protoype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 ssize_t _strlen(const char *string);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 #endif
