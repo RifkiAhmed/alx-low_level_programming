0e36e68ce55516bba7e8cafb5f5ab405a1b96439
ahmed <rifkiahmed.91@gmail.com>
2023-05-26 19:28:37 +0000

    function that create a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,4 +8,5 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 ssize_t _strlen(const char *string);
+int create_file(const char *filename, char *text_content);
 #endif
