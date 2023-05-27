b9f175f10b9bb31de1026741854df52b4b128bca
ahmed <rifkiahmed.91@gmail.com>
2023-05-26 17:44:30 -0700

    fix check

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 ssize_t _strlen(const char *string);
