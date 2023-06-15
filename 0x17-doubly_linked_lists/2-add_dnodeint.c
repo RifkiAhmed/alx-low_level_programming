08812704cbd8059a0bd813d1c34c25ccdf477dc7
ahmed <rifkiahmed.91@gmail.com>
2023-06-15 14:52:34 -0700

    fix betty style checks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,9 +10,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
 
-	node = malloc(dlistint_t *);
+	node = malloc(sizeof(dlistint_t *));
 	if (node == NULL)
-		retrun NULL;
+		return (NULL);
 	node->n = n;
 	node->next = (*head);
 
