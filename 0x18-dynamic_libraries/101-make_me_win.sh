7e06c4491ef825c17098e321bc47421724a390a2
ahmed <rifkiahmed.91@gmail.com>
2023-06-20 02:55:12 +0000

    Fix checks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s -o ~/win_giga.so https://raw.githubusercontent.com/AhmedSeeker/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_giga.so
-export LD_PRELOAD=~/win_giga.so
+curl -o /win_giga.so https://www.github.com/AhmedSeeker/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_giga.so
+export LD_PRELOAD=/win_giga.so
