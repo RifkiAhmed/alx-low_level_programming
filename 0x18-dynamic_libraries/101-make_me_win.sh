18f4ef293b36e6e3acc6b6707590f69855c03cc0
ahmed <rifkiahmed.91@gmail.com>
2023-06-20 02:38:47 +0000

    Delete /

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o win_giga.so /https://github.com/AhmedSeeker/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_giga.so
+curl -o win_giga.so https://github.com/AhmedSeeker/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_giga.so
 export LD_PRELOAD=~/win_giga.so
