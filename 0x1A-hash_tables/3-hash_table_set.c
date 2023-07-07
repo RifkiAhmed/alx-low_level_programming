4e575277708d06280cb819b609739b8dea6f8cab
ahmed <rifkiahmed.91@gmail.com>
2023-07-06 18:47:00 -0700

    Add check for empty string for key and value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL;
 	unsigned long int index;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	node = malloc(sizeof(hash_node_t *));
 	if (node == NULL)
