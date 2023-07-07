b59cdcf4299e050544b70a6e3d79dc349f6d2e7e
ahmed <rifkiahmed.91@gmail.com>
2023-07-06 19:43:47 -0700

    Fix checks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL, *head = NULL;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
