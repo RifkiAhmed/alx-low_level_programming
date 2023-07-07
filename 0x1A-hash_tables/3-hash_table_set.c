6aa2a8e905b254cbe19f79e161787625220a127e
ahmed <rifkiahmed.91@gmail.com>
2023-07-06 19:50:05 -0700

    Fix checks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,13 +24,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(key, head->key))
 			{
+				free(head->value);
 				head->value = strdup(value);
 				return (1);
 			}
 			head = head->next;
 		}
 	}
-	node = malloc(sizeof(hash_node_t *));
+	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
