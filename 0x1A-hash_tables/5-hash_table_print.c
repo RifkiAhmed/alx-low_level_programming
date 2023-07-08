b31d4c11732ddb65dc42b7adaaf1b1abe3068473
ahmed <rifkiahmed.91@gmail.com>
2023-07-08 00:05:23 +0000

    Fix checks

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,18 +8,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index;
 	hash_node_t *head = NULL;
+	unsigned short int node1 = 1;
 
 	if (ht == NULL)
-		printf("{}\n");
+		return;
 	printf("{");
 	for (index = 0; index < ht->size; index++)
 	{
 		if (ht->array[index])
 		{
+			if (!node1)
+				printf(", ");
 			head = ht->array[index];
 			while (head)
 			{
-				printf("'%s': '%s', ", head->key, head->value);
+				printf("'%s': '%s'", head->key, head->value);
+				node1 = 0;
 				head = head->next;
 			}
 			head = NULL;
